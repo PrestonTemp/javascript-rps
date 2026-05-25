4695358040f35cd5476be3eef3b12b564db13639
PrestonTemp <262469725+PrestonTemp@users.noreply.github.com>
2026-05-25 18:39:51 -0400

    Actually finished the game

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let num = Math.floor(Math.random() * 3);
     switch(num){
@@ -7,8 +10,51 @@ function getComputerChoice(){
         case 1:
             return "paper";
             break;
+        case 2:
+            return "scissors";
     }
-    return "scissors"
+    return null;
 }
 
-console.log(getComputerChoice())
+function getHumanChoice(){
+    let ans = prompt("Rock, paper, or scissors?");
+
+    return ans.toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice){
+    if ((humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice == "scissors" && computerChoice == "paper")
+    ){
+        console.log("You win!");
+        humanScore++;
+    }
+    else if(humanChoice == computerChoice){
+        console.log("You Tied!");
+        humanScore += 0.5;
+        computerScore += 0.5;
+    }
+    else{
+        console.log("You lost!");
+        computerScore++;
+    }
+}
+
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if(humanScore > computerScore){
+        console.log("You are the winner!");
+    }
+    else if(humanScore < computerScore){
+        console.log("You are the loser!");
+    }
+    else{
+    console.log("You tied!");
+    }
+}
+
+playGame();
